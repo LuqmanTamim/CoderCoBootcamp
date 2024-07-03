94488e919fee404531aa7b594d0ca4162a6b9ce5
LuqmanTamim <luqmantamim1810@gmail.com>
2024-07-03 19:49:09 +0100

    Update app.py 8080 on docker

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return "Hello world, my name's Luqman. Welcome to my Python app!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
