73e04b3106ee83a54c16d0910ce71b8ac631f1e4
LuqmanTamim <luqmantamim1810@gmail.com>
2024-08-01 11:59:10 +0100

    Update outputs.tf

--- Projects/WordPress_Terraform/outputs.tf
+++ Projects/WordPress_Terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "wordpress_public_ips" {
   description = "The public IP addresses of the WordPress instances"
   value       = { for instance in aws_instance.wordpress_EC2 : instance.id => instance.public_ip }
-}]
+}
